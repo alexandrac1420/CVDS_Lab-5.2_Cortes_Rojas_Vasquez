0534fe263b904d001b27369ac0d60cdd95f0086c
unknown <juansebastianvasquezvega@gmail.com>
2024-03-05 18:12:38 -0500

    Commit Final

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+function HelloWorld() {
+    const [data, setData] = useState(null);
 
+    useEffect(() => {
+        fetch('http://localhost:8080/api/hello')
+            .then(response => response.text())
+            .then(data => setData(data))
+            .catch(error => console.error(error));
+    }, []);
 
-async function HelloWorld() {
-var result;
-var response = await fetch('http://localhost:8080/new')
-  .then(response => response.text())
-  .then(data => {result=data;})
-  .catch(error => console.error(error));
-  return (
-    <div className="HelloWorld">
-        <p>
-            {result}
-        </p>
-    </div>
-  );
+    return (
+        <div className="HelloWorld">
+            {data !== null ? (
+                <p>{data}</p>
+            ) : (
+                <p>Cargando...</p>
+            )}
+        </div>
+    );
 }
+
 export default HelloWorld;
+
 
